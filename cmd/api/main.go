74e6d40e8a500d0374cc1d5a38e3d96c0ced9c50
waditya <waditya02@gmail.com>
2025-03-30 15:52:14 -0500

    Correct the datatype for products struct quantity field

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/waditya/go-mux-basic-rest-api-project/store"
+	"github.com/waditya/go-mux-basic-rest-api-project/internal/store"
 )
 
 func main() {
--- internal/store/products.go
+++ internal/store/products.go
@@ -3,6 +3,6 @@ package store
 type Product struct {
 	Id       int64   `json:"id"`
 	Name     string  `json:"name"`
-	Quantity string  `json:"title"`
+	Quantity int64   `json:"quantity"`
 	Price    float64 `json:"price"`
 }
